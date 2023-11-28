5fff8c4e6db2e9d832d3fa71544bd679c166cd3c
Tjalling-dejong <tjalling.dejong@deltares.nl>
2023-11-28 13:39:43 +0100

    added GEE authentication function

---
+++ EO_Floods/auth.py
@@ -0,0 +1,20 @@
+import os
+import ee
+
+
+def ee_initialize(token_name="EARTHENGINE_TOKE"):
+    """Authenticates Earth Engine and intializes an Earth Engine session."""
+    if ee.data._credentials is None:
+        try:
+            ee_token = os.environ.get(token_name)
+            if ee_token is not None:
+                credentials_file_path = os.path.expanduser("~/.config/earthengine/")
+                if not os.path.exists(credentials_file_path):
+                    credential = '{"refresh_token":"%s"}' % ee_token
+                    os.makedirs(credentials_file_path, exist_ok=True)
+                    with open(credentials_file_path + "credentials", "w") as file:
+                        file.write(credential)
+            ee.Initialize()
+        except Exception:
+            ee.Authenticate()
+            ee.Initialize()
