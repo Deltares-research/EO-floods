98e80376fbbe61ea5711f453cffa4963d7b2fee0
Tjalling de Jong <tjalling.dejong@deltares.nl>
2024-09-03 15:55:26 +0200

    updated gfm tests

--- tests/test_gfm.py
+++ tests/test_gfm.py
@@ -1,15 +1,18 @@
-from EO_Floods.providers import GFM
-from EO_Floods.providers.GFM.auth import GFM_authenticate
 from dotenv import load_dotenv
 import os
 import logging
+import re
+
+from ipyleaflet import Map
 import pytest
+from EO_Floods.providers import GFM
+from EO_Floods.providers.GFM.auth import authenticate_gfm, _get_credentials_from_env
 
 load_dotenv()
 
 
 @pytest.mark.integration
-def test_GFM(caplog, capsys, mocker):
+def test_GFM(caplog, mocker):
     caplog.set_level(logging.INFO)
 
     gfm = GFM(
@@ -20,8 +23,7 @@ def test_GFM(caplog, capsys, mocker):
         pwd=os.getenv("GFM_PWD"),
     )
     # Test init
-    captured = capsys.readouterr()
-    assert "Successfully authenticated to the GFM API\n" in captured.out
+    assert "Successfully authenticated to the GFM API\n" in caplog.text
     assert "Successfully uploaded geometry to GFM server" in caplog.text
     assert "Retrieving GFM product information" in caplog.text
     assert len(gfm.products) == 5
@@ -36,22 +38,50 @@ def test_GFM(caplog, capsys, mocker):
     gfm.select_data(dates=dates[:2])
     assert len(gfm.products) == 2
 
+    # Test view_data
+    wms_map = gfm.view_data()
+    assert isinstance(wms_map, Map)
+
+    # Test export_data
+    mock_get_request = mocker.patch("requests.get")
+    mock_request_response = mocker.Mock(status_code=200, json=lambda: {"link": "mock_link"})
+    mock_get_request.return_value = mock_request_response
+    gfm.export_data()
+    for product in gfm.products:
+        assert f"Image: {product['product_time']}, download link: {{'link': 'mock_link'}}" in caplog.text
+
+    with pytest.raises(ValueError, match="dates should be a list of dates, not <class 'str'>"):
+        gfm.select_data(dates="03-04-1995")
+    
     with pytest.raises(
         ValueError, match="No data found for given date\(s\): 03-04-1995"
     ):
-        gfm.select_data(dates="03-04-1995")
+        gfm.select_data(dates=["03-04-1995"])
+    
 
+    
 
-def test_GFM_auth(mocker, capsys):
+def test_GFM_auth(mocker, caplog):
     mock_post_request = mocker.patch("requests.post")
     mock_response = mocker.Mock(status_code=200, json=lambda: {"test": "data"})
     mock_post_request.return_value = mock_response
-    response = GFM_authenticate(email=os.getenv("GFM_EMAIL"), pwd=os.getenv("GFM_PWD"))
-    captured = capsys.readouterr()
-    assert "Successfully authenticated to the GFM API\n" in captured.out
+    response = authenticate_gfm(email=os.getenv("GFM_EMAIL"), pwd=os.getenv("GFM_PWD"))
+    assert "Successfully authenticated to the GFM API\n" in caplog.text
+    assert isinstance(response, dict)
+
+    response = authenticate_gfm(from_env=True)
+    assert "Successfully authenticated to the GFM API\n" in caplog.text
     assert isinstance(response, dict)
 
     mock_response.status_code = 400
-    GFM_authenticate(email=os.getenv("GFM_EMAIL"), pwd=os.getenv("GFM_PWD"))
-    captured = capsys.readouterr()
-    assert "Incorrect email or password, please try again\n" in captured.out
+    authenticate_gfm(email=os.getenv("GFM_EMAIL"), pwd=os.getenv("GFM_PWD"))
+    assert "Incorrect email or password, please try again\n" in caplog.text
+
+
+def test_GFM_auth_get_credentials_from_env(mocker):
+    email,pwd = _get_credentials_from_env()
+    assert email == os.getenv("GFM_EMAIL")
+    mock_getenv = mocker.patch("os.getenv")
+    mock_getenv.return_value = None
+    with pytest.raises(ValueError, match=re.escape("Environment variables ['GFM_EMAIL', 'GFM_PWD'] not set.")):
+        _get_credentials_from_env()
