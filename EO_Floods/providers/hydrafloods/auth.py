7f574bfb221d754e839b00f057574cebb7353a90
Tjalling de Jong <tjalling.dejong@deltares.nl>
2024-09-03 16:14:24 +0200

    improved hydrafloods auth

--- EO_Floods/providers/hydrafloods/auth.py
+++ EO_Floods/providers/hydrafloods/auth.py
@@ -1,24 +1,27 @@
-import os
+"""Function for authenticatinh to earth engine with environment variables."""
+
 import logging
+import os
+from pathlib import Path
+
 import ee
 
 log = logging.getLogger(__name__)
 
 
-def ee_initialize(token_name="EARTHENGINE_TOKE"):
-    """Authenticates Earth Engine and intializes an Earth Engine session."""
-    if ee.data._credentials is None:
+def ee_initialize(token_name: str = "EARTHENGINE_TOKEN") -> None:  # noqa: S107
+    """Authenticate Earth Engine and intializes an Earth Engine session."""
+    if ee.data._credentials is None:  # noqa: SLF001
         try:
             ee_token = os.environ.get(token_name)
             if ee_token is not None:
-                credentials_file_path = os.path.expanduser("~/.config/earthengine/")
-                if not os.path.exists(credentials_file_path):
-                    credential = '{"refresh_token":"%s"}' % ee_token
-                    os.makedirs(credentials_file_path, exist_ok=True)
-                    with open(credentials_file_path + "credentials", "w") as file:
+                credentials_file_path = Path("~/.config/earthengine/").expanduser()
+                if not credentials_file_path.exists():
+                    credential = '{"refresh_token":"{ee_token}"}'
+                    credentials_file_path.mkdir(parents=True)
+                    with credentials_file_path.joinpath("credentials").open("w") as file:
                         file.write(credential)
             ee.Initialize()
-        except Exception:
+        except Exception:  # noqa: BLE001
             ee.Authenticate()
-            ee.Initialize()
-    ee.Initialize()
+    ee.Initialize(project=os.environ.get("EARTH_ENGINE_PROJECT"))
