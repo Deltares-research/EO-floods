43d738df8eeea17a5995ca85d688c172da905ca8
Tjalling de Jong <tjalling.dejong@deltares.nl>
2024-09-03 11:44:32 +0200

    added docstrings and typing to leaflet.py

--- EO_Floods/providers/GFM/leaflet.py
+++ EO_Floods/providers/GFM/leaflet.py
@@ -1,4 +1,6 @@
-from typing import List
+"""Mapping class for creating WMS ipyleaflet timeseries maps."""
+
+from __future__ import annotations
 
 from ipyleaflet import Map, WidgetControl, WMSLayer, basemaps
 from ipywidgets import SelectionSlider
@@ -10,18 +12,38 @@ WMS_URL = "https://geoserver.gfm.eodc.eu/geoserver/gfm/wms"
 
 
 class TimeWMSLayer(WMSLayer):
+    """Time series wrapper for WMSLayer."""
+
     time = Unicode("").tag(sync=True, o=True)
 
 
-class WMS_MapObject:
+class WMSMap:
+    """Class for creating ipyleaflet WMS maps with a time slider."""
+
     def __init__(
         self,
         start_date: str,
         end_date: str,
-        layers: str,
-        bbox: List[float],
+        layers: str | list[str],
+        bbox: list[float],
         wms_url: str = WMS_URL,
-    ):
+    ) -> None:
+        """Instantiate a WMSMap object.
+
+        Parameters
+        ----------
+        start_date : str
+            start date of timeseries
+        end_date : str
+            end date of timeseries
+        layers : str or list[str]
+            name of map layers
+        bbox : list[float]
+            bounding box in [xmin, ymin, xmax, ymax] format
+        wms_url : str, optional
+            url of the WMS, by default WMS_URL
+
+        """
         self.wms = TimeWMSLayer(
             url=wms_url,
             layers=layers,
@@ -33,7 +55,15 @@ class WMS_MapObject:
         self.end_date = end_date
         self.bbox = bbox
 
-    def get_map(self):
+    def get_map(self) -> Map:
+        """Create a WMS map with a time slider.
+
+        Returns
+        -------
+        Map
+            ipyleaflet map instance
+
+        """
         centroid = get_centroid(self.bbox)
         m = Map(basemap=basemaps.OpenStreetMap.Mapnik, center=centroid, zoom=9)
         m.add(self.wms)
@@ -43,10 +73,9 @@ class WMS_MapObject:
         m.add(slider_cntrl)
         return m
 
-    def _get_slider(self):
+    def _get_slider(self) -> SelectionSlider:
         time_options = get_dates_in_time_range(self.start_date, self.end_date)
-        slider = SelectionSlider(description="Time:", options=time_options)
-        return slider
+        return SelectionSlider(description="Time:", options=time_options)
 
-    def _update_wms(self, change):
+    def _update_wms(self) -> None:
         self.wms.time = self.slider.value
